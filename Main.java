15fd4b0e666a0dc663f7fc786dcbadd36299b35d
Ethan Song <600020389@fjuhsd.org>
2021-02-12 01:04:05 +0000

    Made the methods.

--- Main.java
+++ Main.java
@@ -7,7 +7,20 @@ public class Main
 	public static void main(String[] args)
 	{
 		
-            int[] nums = {7,28,496,1111,199,201,17};
+    int[] nums = {7,28,496,1111,199,201,17};
+    for( int num : nums )
+      {
+        Number one = new Number(num);
+        if (one.isOdd(num))
+	      System.out.print( num + " is odd.\n" );
+        else 
+	      System.out.print( num + " is not odd.\n" );
+	
+        if (one.isPerfect(num))
+	      System.out.print( num + " is perfect.\n" );
+        else 
+	      System.out.print( num + " is not perfect.\n" );
+      }
             //see readme file for additional code
            
 	}
--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Ethan Song
 
 
 //write the Number class
@@ -9,20 +9,77 @@
 //to guide you
 
 //header for Number class
+public class Number{
 
 //private instance variable (must be an Integer)
+private Integer num;
 
 //constructor
+public Number(){
+  num = 0;
+}
 
+public Number(int n){
+  num = n;
+}
 
 //getter method
-
+public Integer getNum(){
+  return num;
+}
 
 //setter method
+public void setNum(int n){
+  num = n;
+}
 
 //isOdd method
+public boolean isOdd(int number){
+
+  //if the modulus of given number divided by 2 has a remainder of 1
+  if(number % 2 == 1){
+	  //return true
+	  return true;
+	}
+
+//return false
+return false;
+
+}
 
 //isPerfect method
+public boolean isPerfect(int number){
+
+  //create integer called divSum set at 0
+  int divSum = 0;
+  //for loop with int x, less than number, adding one per iteration
+  for(int x = 1; x < number; x++){
+
+	  //if the number divided by x has no remainder
+	  if(number % x == 0){
+		//add x to divSum
+		divSum = divSum + x;
+	}
+
+}
+  //if divSum is equal to the number
+  if(divSum == number){
+	//return true
+	return true;
+}
+
+  //else
+  else
+	  //return false
+	  return false;
+
+}
 
 
 //toString
+public String toString(){
+  String output = "Nums: " + num;
+  return output;
+}
+
+}
